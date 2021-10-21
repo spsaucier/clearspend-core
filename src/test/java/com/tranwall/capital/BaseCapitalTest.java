d3650db94a1067dad34c1af8275b5aa1971691b5
Vyacheslav Akimov <slava.akimov@tranwall.com>
2021-10-21 12:52:31 +0000

    Merged in test_fix (pull request #29)
    Workaround to make tests work

--- src/test/java/com/tranwall/capital/BaseCapitalTest.java
+++ src/test/java/com/tranwall/capital/BaseCapitalTest.java
@@ -7,12 +7,18 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.springtest.MockServerTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @CapitalTest
 @Testcontainers
+// It seems that mock server leads to situation when new context is spawned
+// for every test class leading to a situation when db stops accepting any new
+// connections. Marking the context as dirty is a workaround to avoid it
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 @MockServerTest("server.url=http://localhost:${mockServerPort}")
 public abstract class BaseCapitalTest {
 
