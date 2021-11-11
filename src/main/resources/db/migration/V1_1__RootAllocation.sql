d84ea81ccbecb83cc900db817059ed41dbacec8d
Vyacheslav Akimov <slava.akimov@tranwall.com>
2021-11-11 19:18:45 +0000

    Merged in db_script_fix (pull request #77)
    New columns default data should be set for all the existing db data
    
    * New columns default data should be set for all the existing db data
    
    
    Approved-by: George Buduianu

--- src/main/resources/db/migration/V1_1__RootAllocation.sql
+++ src/main/resources/db/migration/V1_1__RootAllocation.sql
@@ -5,11 +5,15 @@ update program set card_type = 'PLASTIC' where id = '033955d1-f18e-497e-9905-88b
 alter table program alter column card_type set not null;
 
 -- Card table changes
-alter table card add column card_type varchar(20) not null;
+alter table card add column card_type varchar(20);
+update card set card_type = 'VIRTUAL' where card_type is null;
+alter table card alter column card_type set not null;
 
 -- Allocation table changes
 alter table allocation drop column program_id;
-alter table allocation add column i2c_stakeholder_ref varchar(50) not null;
+alter table allocation add column i2c_stakeholder_ref varchar(50);
+update allocation set i2c_stakeholder_ref = 'I2C_STAKEHOLDER_ID' where i2c_stakeholder_ref is null;
+alter table allocation alter column i2c_stakeholder_ref set not null;
 
 drop index if exists allocation_idx1;
 create index allocation_idx1 on allocation (business_id);
