c2cbfdefc8a237da15e9fde9d0fd9ac1969394f3
Tim Kuchlein <tim.kuchlein@clearspend.com>
2021-12-07 18:24:20 +0000

    Merged in feature/CAP-214-dovetail-api-requests.9 (pull request #115)
    ImageController debugging for receipt upload

--- src/main/java/com/tranwall/capital/controller/ImageController.java
+++ src/main/java/com/tranwall/capital/controller/ImageController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,9 +36,9 @@ public class ImageController {
 
   private final ReceiptService receiptService;
 
-  @PostMapping("/receipts")
-  private CreateReceiptResponse storeReceiptImage(
-      @RequestParam("receipt") MultipartFile receiptFile) throws IOException {
+  @PostMapping(value = "/receipts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  private CreateReceiptResponse storeReceiptImage(@RequestPart("receipt") MultipartFile receiptFile)
+      throws IOException {
     CurrentUser currentUser = CurrentUser.get();
     final Receipt receipt =
         receiptService.storeReceiptImage(
