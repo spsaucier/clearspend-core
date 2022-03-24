ad30af63f790bad0627ddceaf5849f35c5c10cc9
Slava Akimov <slava.akimov@clearspend.com>
2022-03-24 13:56:20 +0000

    [CAP-572] Old reallocation activity records without users should be retrieved correctly
    Approved-by: jim.scarborough

--- src/main/java/com/clearspend/capital/data/model/embedded/UserDetails.java
+++ src/main/java/com/clearspend/capital/data/model/embedded/UserDetails.java
@@ -3,7 +3,7 @@ package com.clearspend.capital.data.model.embedded;
 import com.clearspend.capital.common.masking.annotation.Sensitive;
 import com.clearspend.capital.common.typedid.data.TypedId;
 import com.clearspend.capital.common.typedid.data.UserId;
-import com.clearspend.capital.crypto.data.model.embedded.RequiredEncryptedStringWithHash;
+import com.clearspend.capital.crypto.data.model.embedded.NullableEncryptedStringWithHash;
 import com.clearspend.capital.data.model.User;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -23,18 +23,23 @@ import org.hibernate.annotations.Type;
 @MappedSuperclass
 public class UserDetails {
 
+  @NonNull
   @JoinColumn(referencedColumnName = "id", table = "user")
   @Column(updatable = false)
   @Type(type = "com.clearspend.capital.common.typedid.jpatype.TypedIdJpaType")
   private TypedId<UserId> id;
 
-  @NonNull @Sensitive @Embedded private RequiredEncryptedStringWithHash firstName;
+  @NonNull @Sensitive @Embedded private NullableEncryptedStringWithHash firstName;
 
-  @NonNull @Sensitive @Embedded private RequiredEncryptedStringWithHash lastName;
+  @NonNull @Sensitive @Embedded private NullableEncryptedStringWithHash lastName;
 
-  @NonNull @Sensitive @Embedded private RequiredEncryptedStringWithHash email;
+  @NonNull @Sensitive @Embedded private NullableEncryptedStringWithHash email;
 
   public static UserDetails of(User user) {
-    return new UserDetails(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail());
+    return new UserDetails(
+        user.getId(),
+        new NullableEncryptedStringWithHash(user.getFirstName().getEncrypted()),
+        new NullableEncryptedStringWithHash(user.getLastName().getEncrypted()),
+        new NullableEncryptedStringWithHash(user.getEmail().getEncrypted()));
   }
 }
