a9d7dba5556e2120f296c158c2562ed1c94f2f44
Max Hamilton <maxh@theodo.com>
2022-04-11 15:32:15 +0000

    Merged in feature/AI-187-COA-consolidation (pull request #512)
    Consolidated ChartOfAccounts controllers
    
    * Consolidated ChartOfAccounts controllers
    
    
    Approved-by: patrick.morton

--- src/main/java/com/clearspend/capital/controller/ChartOfAccountsController.java
+++ src/main/java/com/clearspend/capital/controller/ChartOfAccountsController.java
@@ -1,13 +1,20 @@
 package com.clearspend.capital.controller;
 
 import com.clearspend.capital.client.codat.types.CodatAccountNestedResponse;
+import com.clearspend.capital.controller.type.chartOfAccounts.AddChartOfAccountsMappingRequest;
+import com.clearspend.capital.controller.type.chartOfAccounts.GetChartOfAccountsMappingResponse;
 import com.clearspend.capital.data.model.ChartOfAccounts;
+import com.clearspend.capital.service.ChartOfAccountsMappingService;
 import com.clearspend.capital.service.ChartOfAccountsService;
 import com.clearspend.capital.service.type.CurrentUser;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ChartOfAccountsController {
   private final ChartOfAccountsService chartOfAccountsService;
+  private final ChartOfAccountsMappingService mappingService;
 
-  @GetMapping("/")
+  @GetMapping("/stored")
   CodatAccountNestedResponse getChartOfAccountsForBusiness() {
     return new CodatAccountNestedResponse(
         chartOfAccountsService
@@ -34,5 +42,24 @@ public class ChartOfAccountsController {
   @GetMapping("/total-changes")
   Integer getTotalChartOfAccountsChangesForBusiness() {
     return chartOfAccountsService.getTotalChangesForBusiness(CurrentUser.getBusinessId());
+  }
+
+  @GetMapping("/mappings")
+  GetChartOfAccountsMappingResponse getMappings() {
+    return new GetChartOfAccountsMappingResponse(
+        mappingService.getAllMappingsForBusiness(CurrentUser.getBusinessId()));
+  }
+
+  @PostMapping("/mappings")
+  GetChartOfAccountsMappingResponse addChartOfAccountsMappings(
+      @Validated @RequestBody List<AddChartOfAccountsMappingRequest> request) {
+    return new GetChartOfAccountsMappingResponse(
+        mappingService.addChartOfAccountsMappings(CurrentUser.getBusinessId(), request));
+  }
+
+  @DeleteMapping("/mappings")
+  Boolean deleteChartOfAccountsMappings() {
+    mappingService.deleteChartOfAccountsMappingsForBusiness(CurrentUser.getBusinessId());
+    return true;
   }
 }
