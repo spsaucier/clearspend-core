526d3017ca5daf94173a597d73338070efb9e6d6
Craig Miller <craig.miller@clearspend.com>
2022-04-19 20:11:22 +0000

    Merged in feature/CAP-445 (pull request #539)
    CAP-445 - ReceiptImageService permissions
    
    * Downgraded a bunch of methods to pacakge-private
    
    * Merged master into feature/CAP-445
    
    Approved-by: jim.scarborough

--- src/main/java/com/clearspend/capital/service/ReceiptImageService.java
+++ src/main/java/com/clearspend/capital/service/ReceiptImageService.java
@@ -5,7 +5,6 @@ import com.clearspend.capital.common.typedid.data.ReceiptId;
 import com.clearspend.capital.common.typedid.data.TypedId;
 import com.clearspend.capital.common.typedid.data.UserId;
 import com.clearspend.capital.common.typedid.data.business.BusinessId;
-import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +18,7 @@ public class ReceiptImageService {
   private final GoogleCloudStorageClient googleCloudStorageClient;
 
   // creates new receipt record and uploads receipt image to GCS (Google Cloud Storage)
-  public String storeReceiptImage(
+  String storeReceiptImage(
       TypedId<BusinessId> businessId,
       TypedId<UserId> userId,
       TypedId<ReceiptId> receiptId,
@@ -34,16 +33,15 @@ public class ReceiptImageService {
   }
 
   // returns data held in GCS (Google Cloud Storage)
-  public byte[] getReceiptImage(String receiptPath) {
+  byte[] getReceiptImage(String receiptPath) {
     return googleCloudStorageClient.readFile(receiptPath);
   }
 
-  public boolean deleteReceiptImage(String receiptPath) {
+  boolean deleteReceiptImage(String receiptPath) {
     return googleCloudStorageClient.deleteFile(receiptPath);
   }
 
-  @VisibleForTesting
-  public String getReceiptPath(
+  String getReceiptPath(
       TypedId<BusinessId> businessId, TypedId<UserId> userId, TypedId<ReceiptId> receiptId) {
     return String.format("/receipts/bid_%s/uid_%s/rid_%s", businessId, userId, receiptId);
   }
