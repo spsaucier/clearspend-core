ecaa6fcb156223099785dc0389c1447bf5364f2f
Slava Akimov <slava.akimov@clearspend.com>
2022-06-01 16:38:11 +0000

    [CAP-1222] Flyway migration script fix
    Approved-by: Buduianu George

--- src/main/resources/db/migration/V1_20220601__Deposit_Adjustments_Effective_Date_Fix.sql
+++ src/main/resources/db/migration/V1_20220601__Deposit_Adjustments_Effective_Date_Fix.sql
@@ -1,4 +1,5 @@
-update adjustment set
-  effective_date = (select a.visible_after from account_activity a where a.adjustment_id = adjustment_id),
-  version = version + 1
-  where type = 'DEPOSIT'
+update adjustment
+   set effective_date = case when aa.visible_after is null then effective_date else aa.visible_after end,
+   version = adjustment.version + 1
+  from account_activity aa
+ where adjustment.id = aa.adjustment_id and adjustment.type = 'DEPOSIT' and aa.type = 'BANK_DEPOSIT_STRIPE'
